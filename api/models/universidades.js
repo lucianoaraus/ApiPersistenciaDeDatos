389c5c9c3f3838e87920c950eba7ce7dafd86909
unknown <tomasmiranda98@hotmail.com>
2021-11-05 21:09:20 -0300

    se arregla error en modulo universidad

--- api/models/universidades.js
+++ api/models/universidades.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
   
   universidades.associate = function (models) {
     
-    materia.belongsTo(models.carrera, {
+    universidades.belongsTo(models.carrera, {
       // materia pertenece al modelo carrera
       as: "Carrera-Relacionada", // nombre de mi relacion
       foreignKey: "id_carrera", // campo con el que voy a igualar
